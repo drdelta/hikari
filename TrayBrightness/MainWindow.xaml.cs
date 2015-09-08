4a5b264e169279dec4e9cb83edcd62a48cd0733e
darkemperor8 <darkemperor8@gmail.com>
2015-09-08 12:32:05 +0200

    better implementation of NotifyIcon in WPF

--- TrayBrightness/MainWindow.xaml.cs
+++ TrayBrightness/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using TrayBrightness.Models.Display;
 using TrayBrightness.Win32;
 using MahApps.Metro.Controls;
+using Hardcodet.Wpf.TaskbarNotification;
 
 namespace TrayBrightness
 {
@@ -24,8 +25,10 @@ namespace TrayBrightness
     /// https://code.google.com/p/monitorprofiler/source/browse/
     /// mahapp.metro style
     /// http://mahapps.com/guides/quick-start.html
+    /// hardcodet notifyicon for wpf
+    /// http://www.hardcodet.net/wpf-notifyicon
     /// </summary>
-    
+
     public partial class MainWindow : MetroWindow
     {
         private readonly MonitorCollection _monitorCollection = new MonitorCollection();
@@ -35,7 +38,10 @@ namespace TrayBrightness
         public MainWindow()
         {
             InitializeComponent();
-            this.Hide();
+            //this.Hide();
+            this.WindowState = WindowState.Normal;
+            this.Show();
+
             /// Enumerate Displays
             var MyInfoEnumProc = new NativeMethods.MonitorEnumDelegate(MonitorEnum);
             NativeMethods.EnumDisplayMonitors(IntPtr.Zero, IntPtr.Zero, MyInfoEnumProc, IntPtr.Zero);
@@ -44,10 +50,10 @@ namespace TrayBrightness
             var desktopWorkingArea = SystemParameters.WorkArea;
             this.Left = desktopWorkingArea.Right - this.Width;
             this.Top = desktopWorkingArea.Bottom - this.Height;
-
+/*
             /// Create a tray icon & menu
             System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon();
-            System.Windows.Forms.ContextMenu cm = new System.Windows.Forms.ContextMenu();
+            System.Windows.Forms.ContextMenu ContextMenu1 = new System.Windows.Forms.ContextMenu();
             System.Windows.Forms.MenuItem mi1 = new System.Windows.Forms.MenuItem();
             System.Windows.Forms.MenuItem mi2 = new System.Windows.Forms.MenuItem();
             ni.Icon = new System.Drawing.Icon("main.ico");
@@ -67,29 +73,45 @@ namespace TrayBrightness
 
                     }
                 };
-            ni.ContextMenu = cm;
-            cm.MenuItems.AddRange(
-                    new System.Windows.Forms.MenuItem[] { mi2 });
-            mi2.Index = 0;
-            mi2.Text = "C&onfiguration";
-            cm.MenuItems.AddRange(
+            ni.ContextMenu = ContextMenu1;
+            ContextMenu1.MenuItems.AddRange(
                     new System.Windows.Forms.MenuItem[] { mi1 });
-            mi1.Index = 1;
-            mi1.Text = "E&xit";
+            mi1.Index = 0;
+            mi1.Text = "S&ettings";
             mi1.Click +=
                 delegate (object sender, EventArgs args)
                 {
+                };
+            ContextMenu1.MenuItems.AddRange(
+                    new System.Windows.Forms.MenuItem[] { mi2 });
+            mi2.Index = 1;
+            mi2.Text = "E&xit";
+            mi2.Click +=
+                delegate (object sender, EventArgs args)
+                {
+                    this.Hide();
+                    ni.Visible = false;
                     Application.Current.Shutdown();
                 };
-
+*/                
         }
+
 
         /////// TODO //////////
         // COLOR change to aero/ metro
+        // READ sensor data for AUTO adjustment
+        // create CONFIG window
+        //// choose monitor
+        //// calibration between monitors
+        //// light sensor settings
 
-        // TRAY icon and CONTROLS
-        //      Config
-        //      Exit
+        private TaskbarIcon tb;
+
+        private void InitApplication()
+        {
+            //initialize NotifyIcon
+            tb = (TaskbarIcon)FindResource("MyNotifyIcon");
+        }
 
         protected override void OnStateChanged(EventArgs e)
         {
